fc39c968a11d4c43947605b78477956ffe6d17eb
Francesco Mabilia <43805799+francymab@users.noreply.github.com>
2023-06-16 16:44:50 +0200

    Update id_sottoscrizione_trigger.sql

--- Triggers/id_sottoscrizione_trigger.sql
+++ Triggers/id_sottoscrizione_trigger.sql
@@ -1,6 +1,6 @@
 --ID SOTTOSCRIZIONE UTENTE--
-CREATE OR REPLACE TRIGGER id_sottoscrizione_trigger BEFORE
-INSERT ON sottoscrizione
+CREATE OR REPLACE TRIGGER id_sottoscrizione_trigger 
+BEFORE INSERT ON sottoscrizione
 FOR EACH ROW
 BEGIN
     -- Inseriamo come nuovo id sottoscrizione il valore successivo della sequenza --
