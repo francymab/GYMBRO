7cd857830537113d917df9f3687c4e0140ad901c
Francesco Mabilia <43805799+francymab@users.noreply.github.com>
2023-06-16 16:42:25 +0200

    Update id_scontrino_trigger.sql

--- Triggers/id_scontrino_trigger.sql
+++ Triggers/id_scontrino_trigger.sql
@@ -1,6 +1,6 @@
 --ID SCONTRINO SCONTRINO--
-CREATE OR REPLACE TRIGGER id_scontrino_trigger BEFORE
-INSERT ON scontrino
+CREATE OR REPLACE TRIGGER id_scontrino_trigger 
+BEFORE INSERT ON scontrino
 FOR EACH ROW
 BEGIN
     -- Inseriamo come nuovo id scontrino il valore successivo della sequenza --
